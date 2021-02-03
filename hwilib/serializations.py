2ddb66f1040ba868312404632b98fd5769b3817c
jackielove4u <jackielove4u@hotmail.com>
2021-02-03 10:49:10 +0100

    Fix bytes for groestlcoin_hash needed for Ledger

--- hwilib/serializations.py
+++ hwilib/serializations.py
@@ -66,7 +66,7 @@ def hash160(s: bytes) -> bytes:
     return ripemd160(sha256(s))
 
 def groestl(s: bytes) -> bytes:
-    return groestlcoin_hash.getHash(s, len(s))
+    return groestlcoin_hash.getHash(bytes(s), len(s))
 
 class AddressType(Enum):
     PKH = 1
